4e8fa428cf1f91cd81fabbfe583da0d1b524c5a5
Nikola Čunović <nikola@Nikolas-Mini.home>
2024-08-18 22:22:04 +0200

    inital commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Well to be honest, I really like this docker shit!
         </p>
         <a
           className="App-link"
